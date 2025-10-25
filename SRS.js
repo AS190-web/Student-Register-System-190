cf4e54d676215b91913808d5925afe98fa68a373
AS190-web <aysswain06@gmail.com>
2025-10-25 12:06:50 +0530

    Add files via upload

---
+++ SRS.js
@@ -0,0 +1,4 @@
+document.getElementById("enquiryForm").addEventListener("submit" , function (e) {e.preventDefault();
+    alert("Thank you for your enquiry! We'll get back to you soon.")
+    this.reset();
+});
